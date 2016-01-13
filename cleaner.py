146cf05129ec2b8e8dc544396b387765b8e14074
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-01-14 04:03:52 +0530

    all stocks cleaning program written

--- cleaner.py
+++ cleaner.py
@@ -1,7 +1,14 @@
 import re
 import csv
+
 with open('data/NSE-datasets-codes.csv', 'r') as inp, open('data/NSE_datasets-codes-cleaned.csv', 'w') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
-        if ~bool(re.search('CNX', row[1])):
+        if "NSE/CNX" not in row[0] and "NSE/NIFTY" not in row[0] and "NSE/SPCNX" not in row[0]:
+            writer.writerow(row)
+
+with open('data/all-stocks.csv', 'r',-1,"utf-8") as inp, open('data/all-stocks-cleaned.csv', 'w') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+        if "Date" not in row[0]:
             writer.writerow(row)
---
+++ fetcher.py
@@ -0,0 +1,23 @@
+import re
+import csv
+
+from itertools import count
+import requests
+
+HEADERS = {'user-agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+URL = "http://www.example.com/img%03d.png"
+
+# with a session, we get keep alive
+session = requests.session()
+
+with open('data/NSE_datasets-codes-cleaned', 'r') as inp, open('data/all-stocks.csv', 'w') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+            full_url = "https://www.quandl.com/api/v3/datasets/"+row[0]+".csv?auth_token=qPvscoNKm8eS7cjipW7k&collapse=quarterly"
+            ignored, filename = URL.rsplit('/', 1)
+
+            response = session.get(full_url, headers=HEADERS)
+            if not response.ok:
+                break
+            writer.write(response.content)
+
