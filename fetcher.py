6c228d6b0f13d4e2ac72fb1c75089eba3b2b8d1c
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-01-15 02:11:45 +0530

    fetcher is working

--- fetcher.py
+++ fetcher.py
@@ -10,8 +10,8 @@ URL = "http://www.example.com/img%03d.png"
 # with a session, we get keep alive
 session = requests.session()
 
-with open('data/NSE_datasets-codes-cleaned', 'r') as inp, open('data/all-stocks.csv', 'w') as out:
-    writer = csv.writer(out)
+with open('data/NSE_datasets-codes-cleaned.csv', 'r') as inp, open('data/all-stocks.csv', 'wb') as out:
+    # writer = csv.writer(out)
     for row in csv.reader(inp):
             full_url = "https://www.quandl.com/api/v3/datasets/"+row[0]+".csv?auth_token=qPvscoNKm8eS7cjipW7k&collapse=quarterly"
             ignored, filename = URL.rsplit('/', 1)
@@ -19,5 +19,6 @@ with open('data/NSE_datasets-codes-cleaned', 'r') as inp, open('data/all-stocks.
             response = session.get(full_url, headers=HEADERS)
             if not response.ok:
                 break
-            writer.write(response.content)
+            out.write(response.content)
+
 
