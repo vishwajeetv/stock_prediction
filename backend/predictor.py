bfe85a155d11e272a87aebd392326b3e2e1034c3
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-03-18 17:52:46 +0530

    refactored, extracted several methods

--- backend/predictor.py
+++ backend/predictor.py
@@ -7,69 +7,78 @@ import matplotlib.pyplot as plotter
 import pandas
 import csv
 
-# stock = Quandl.get("NSE/INFY")
-# stock = np.array(stock)
-# currentRatio = Quandl.get("DEB/INFY_A_CRATIO")
 
-# reader = pandas.io.parsers.read_csv("data/all-stocks-cleaned.csv")
-reader = pandas.io.parsers.read_csv("data/TATAMOTORS/NSE-TATAMOTORS.csv")
-reader = reader.fillna(method='ffill')
-stock = np.array(reader)
-
-openingPrice = stock[:, 1]
-closingPrice = stock[:, 5]
-
-openingPriceTrain, openingPriceTest, closingPriceTrain, closingPriceTest = \
-train_test_split(openingPrice, closingPrice, test_size=0.25, random_state=42)
-
-
-openingPriceTrain = np.reshape(openingPriceTrain,(openingPriceTrain.size,1))
-
-closingPriceTrain = np.reshape(closingPriceTrain,(closingPriceTrain.size,1))
+def readData():
+    # stock = Quandl.get("NSE/INFY")
+    # stock = np.array(stock)
+    # currentRatio = Quandl.get("DEB/INFY_A_CRATIO")
+    # reader = pandas.io.parsers.read_csv("data/all-stocks-cleaned.csv")
+    # reader = pandas.read_csv("data/TATAMOTORS/NSE-TATAMOTORS.csv",index_col='Date',parse_dates = True )
+    df = pandas.read_csv("data/TATAMOTORS/NSE-TATAMOTORS.csv", parse_dates=True, index_col='Date',
+                         usecols=['Date', 'Close'])
+    df = df.fillna(method='ffill')
+    # df = df.dropna()
+    dfAll = pandas.read_csv("data/TATAMOTORS/NSE-TATAMOTORS.csv", parse_dates=True, index_col='Date')
+    dfAll = dfAll.fillna(method='ffill')
+    stock = np.array(dfAll)
+    # plotter.xlabel('Date')
+    # plotter.ylabel('Close')
+    return df, stock
 
 
-openingPriceTest = np.reshape(openingPriceTest,(openingPriceTest.size,1))
-closingPriceTest = np.reshape(closingPriceTest,(closingPriceTest.size,1))
+def plot(df):
+    rollingMean = pandas.rolling_mean(df['Close'], window=100)
+    rollingStdv = pandas.rolling_std(df['Close'], window=100)
+    plotter.plot(rollingMean)
+    # plotting bollinger bands
+    plotter.plot(rollingMean + rollingStdv * 2)
+    plotter.plot(rollingMean - rollingStdv * 2)
+    # df1 = df[['Date','Close']]
+    plotter.plot(df)
+    plotter.show()
 
 
-# regression = linear_model.LinearRegression()
-#
-# regression.fit(openingPriceTrain, closingPriceTrain)
-#
-#
-# predicted = regression.predict(openingPriceTest)
-#
-# plotter.scatter(openingPriceTest, closingPriceTest,  color='black')
-# plotter.plot(closingPriceTest, regression.predict(openingPriceTest))
-# plotter.show()
+def sample(stock):
+    openingPrice = stock[:, 1]
+    closingPrice = stock[:, 5]
+    openingPriceTrain, openingPriceTest, closingPriceTrain, closingPriceTest = \
+        train_test_split(openingPrice, closingPrice, test_size=0.25, random_state=42)
+    openingPriceTrain = np.reshape(openingPriceTrain, (openingPriceTrain.size, 1))
+    closingPriceTrain = np.reshape(closingPriceTrain, (closingPriceTrain.size, 1))
+    openingPriceTest = np.reshape(openingPriceTest, (openingPriceTest.size, 1))
+    closingPriceTest = np.reshape(closingPriceTest, (closingPriceTest.size, 1))
+
+    sampledData = {"openingPriceTrain":openingPriceTrain, "closingPriceTrain":closingPriceTrain,
+                   "openingPriceTest":openingPriceTest, "closingPriceTest":closingPriceTest}
+    return sampledData
 
 
-clf = svm.LinearSVR()
-# clf = linear_model.BayesianRidge()
-clf.fit(openingPriceTrain, closingPriceTrain)
-predicted2 = clf.predict(openingPriceTest)
-# score = clf.fit(openingPriceTrain, closingPriceTrain).score(openingPriceTest,closingPriceTest)
-# print(score)
-# print(cross_validation.cross_val_score(clf,openingPriceTest,closingPriceTest))
-# fig, ax = plotter.subplots()
-# ax.scatter(openingPriceTrain, closingPriceTrain)
-# ax.scatter(closingPriceTest, clf.predict(openingPriceTest))
-# ax.set_xlabel('Measured')
-# ax.set_ylabel('Predicted')
-# plotter.show()
-openingPriceToPredict = np.array([335.50])
-print(clf.predict(openingPriceToPredict))
+def predict(data):
 
-# mlpr = neural_network.MLPRegressor()
-# mlpr.fit(openingPriceTrain, closingPriceTrain)
-# predicted2 = mlpr.predict(openingPriceTest)
-# score = mlpr.fit(openingPriceTrain, closingPriceTrain).score(openingPriceTest,closingPriceTest)
-# print(score)
-# print(cross_validation.cross_val_score(mlpr,openingPriceTest,closingPriceTest))
-# plotter.scatter(openingPriceTest, closingPriceTest,  color='black')
-# plotter.scatter(closingPriceTest, mlpr.predict(openingPriceTest))
-# plotter.show()
-# openingPriceToPredict = np.array([335.50])
-# print(mlpr.predict(openingPriceToPredict))
+    openingPriceTrain, openingPriceTest, closingPriceTrain, closingPriceTest = \
+        data["openingPriceTrain"], data["openingPriceTest"], data["closingPriceTrain"], data["closingPriceTest"]
+    clf = svm.LinearSVR()
+    clf.fit(openingPriceTrain, closingPriceTrain)
+    predicted2 = clf.predict(openingPriceTest)
+    score = clf.fit(openingPriceTrain, closingPriceTrain).score(openingPriceTest, closingPriceTest)
+    print(score)
+    fig, ax = plotter.subplots()
+    ax.scatter(openingPriceTrain, closingPriceTrain)
+    ax.scatter(closingPriceTest, clf.predict(openingPriceTest))
+    ax.set_xlabel('Measured')
+    ax.set_ylabel('Predicted')
+    plotter.show()
+    openingPriceToPredict = np.array([335.50])
+    print(clf.predict(openingPriceToPredict))
+    return clf.predict(np.array([openingPriceToPredict]))
 
-print(clf.predict( np.array([openingPrice])))
+
+if __name__ == "__main__":
+
+    df, stock = readData()
+
+    # plot(df)
+
+    sampledData = sample(stock)
+
+    predict(sampledData)
